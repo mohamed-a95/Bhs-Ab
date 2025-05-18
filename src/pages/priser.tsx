fe0fe09cacf0ceb9bc3d220b91d10168272704c7
mohamed-a95 <mohamedabdihakima95@gmail.com>
2025-05-18 19:30:16 +0200

    changes

--- src/pages/priser.tsx
+++ src/pages/priser.tsx
@@ -4,7 +4,7 @@ const Priser = () => {
   return (
     <>
       {/* Hero Section */}
-      <section className="bg-primary bg-opacity-95 text-white py-12">
+      <section className="bg-grey-200 bg-opacity-95 text-neutral-600 py-12">
         <div className="container mx-auto px-4">
           <div className="max-w-3xl mx-auto text-center">
             <h1 className="text-3xl md:text-4xl font-bold mb-4">Prislista</h1>
