246fe164ac2670f64901e0b46436b6a1baa045da
mohamed-a95 <mohamedabdihakima95@gmail.com>
2025-06-16 15:35:32 +0200

    changes

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,8 @@ import { GtmProvider } from "./Provider/Gtmprovider";
 // Hämta root-element
 const rootElement = document.getElementById("root")!;
 
+console.log("🌍 import.meta.env:", import.meta.env);
+
 // Wrappa App med GtmProvider
 createRoot(rootElement).render(
   <GtmProvider>
